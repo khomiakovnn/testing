43b12988be4bf43a90c836f1ccfb6f508c17e44f
Nikolay Khomiakov <nikolay.khomiakov@gmail.com>
2023-11-06 13:51:18 -0500

    front done

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
+[![Build status](https://ci.appveyor.com/api/projects/status/9f05cqu58ww8w4ku?svg=true)](https://ci.appveyor.com/project/khomiakovnn/testing)
 
---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'docs'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+    }),
+  ],
+};
+
